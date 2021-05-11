a8221b966694b7389095402243fd0cb5ddda9acf
zcx <zcx@umich.edu>
2021-05-10 20:52:36 -0400

    check new annotation

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ downloaddir="$rootdir/download"
 wget http://release.geneontology.org/2020-06-01/ontology/go-basic.obo -O $datadir/go-basic.obo
 wget ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/old/UNIPROT/goa_uniprot_all.gaf.198.gz -O $downloaddir/goa_uniprot_all.gaf.198.gz
 wget ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/old/UNIPROT/goa_uniprot_all.gaf.203.gz -O $downloaddir/goa_uniprot_all.gaf.203.gz
-if [ ! -s "goa_uniprot_all.gaf.203.gz" ];then
+if [ ! -s "$downloaddir/goa_uniprot_all.gaf.203.gz" ];then
     wget ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/goa_uniprot_all.gaf.gz -O $downloaddir/goa_uniprot_all.gaf.203.gz
 fi
 
