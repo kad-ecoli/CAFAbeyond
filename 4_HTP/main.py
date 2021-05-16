f7a9b54adff5f7c983a335b07aa821871d12690b
zcx <zcx@xenial>
2021-05-15 21:25:25 -0400

    target num

--- 4_HTP/main.py
+++ 4_HTP/main.py
@@ -4,6 +4,7 @@ This script compares EXP and HTP annotations
 '''
 import sys
 import os
+curdir =os.path.dirname(os.path.abspath(__file__))
 rootdir=os.path.dirname(curdir)
 datadir=os.path.join(rootdir,"data")
 
@@ -19,17 +20,42 @@ def read_ic(naive_file_list):
         fp.close()
     return ic_dict
 
-def read_target_type_dict(datadir):
-    target_type_dict=dict(NK=dict(),LK=dict(),PK=dict())
-    for target_type in target_type_dict:
-        for Aspect in "FPC":
-            filename="%s/%s.%s"%(datadir,target_type,Aspect)
-            fp=open(filename,'r')
-            lines=fp.read().splitlines()
-            fp.close()
-            target_type_dict[target_type][Aspect]=[line.split()[0] for line in lines]
-    return target_type_dict
+def read_target_dict(goa_file_list):
+    target_list=[]
+    GOterm_dict=dict()
+    for goa_file in goa_file_list:
+        fp=open(goa_file)
+        for line in fp.read().splitlines():
+            items      =line.split('\t')
+            target     =items[0]
+            GOterm_list=items[1].split(',')
+            if len(items)>2:
+                oldGOterm_set=set(items[2].split(','))
+                GOterm_list=[GOterm for GOterm in GOterm_list \
+                      if not GOterm in oldGOterm_set]
+            target_list.append(target)
+            for GOterm in GOterm_list:
+                if not GOterm in GOterm_dict:
+                    GOterm_dict[GOterm]=[target]
+                else:
+                    GOterm_dict[GOterm].append(target)
+        fp.close()
+    target_set=set(target_list)
+    for GOterm in GOterm_dict:
+        GOterm_dict[GOterm]=len(GOterm_dict[GOterm])
+    return target_set,GOterm_dict
 
 if __name__=="__main__":
     ic_dict=read_ic((datadir+"/naive.F", datadir+"/naive.P",
                      datadir+"/naive.C"))
+    print("| Evidence | MF targets | BP targets | CC targets |")
+    print("| :--:     | :--:       | :--:       | :--:                      |")
+    for evidence in ["EXP","HTP","EXP+HTP"]:
+        target_set,GOterm_dict=read_target_dict(
+            ["%s/%s.%s"%(curdir,evidence,Aspect) for Aspect in "FPC"])
+        terms_ge10 =sum([num>=10 for num in GOterm_dict.values()])
+        terms_ge100=sum([num>=100 for num in GOterm_dict.values()])
+        total_ic=sum([ic_dict[GOterm]*num for GOterm,num in GOterm_dict.items() if GOterm in ic_dict])
+        mean_ic=total_ic/sum(GOterm_dict.values())
+        print("| %s | %d | %d | %d | %d | %.3f |"%(
+            evidence, len(target_set), len(GOterm_dict), terms_ge10, terms_ge100, mean_ic))
--- 4_HTP/main.sh
+++ 4_HTP/main.sh
@@ -12,51 +12,34 @@ old=198
 new=203
 for version in `echo $old $new`;do
     $bindir/propagate_gaf_terms.py $datadir/go-basic.obo \
-            $datadir/goa_uniprot_all.EXP.${version}.gz   \
+	    $datadir/goa_uniprot_all.EXP.${version}.gz   \
 	    EXP.F.is_a.$version EXP.P.is_a.$version EXP.C.is_a.$version
     $bindir/propagate_gaf_terms.py $datadir/go-basic.obo \
-        $datadir/goa_uniprot_all.HTP.${version}.gz       \
-        HTP.F.is_a.$version HTP.P.is_a.$version HTP.C.is_a.$version
+	$datadir/goa_uniprot_all.HTP.${version}.gz       \
+	HTP.F.is_a.$version HTP.P.is_a.$version HTP.C.is_a.$version
     zcat $datadir/goa_uniprot_all.EXP.${version}.gz      \
-         $datadir/goa_uniprot_all.HTP.${version}.gz |gzip - > EXP+HTP.${version}.gz
+	 $datadir/goa_uniprot_all.HTP.${version}.gz |gzip - > EXP+HTP.${version}.gz
     $bindir/propagate_gaf_terms.py $datadir/go-basic.obo \
-        EXP+HTP.${version}.gz \
-        EXP+HTP.F.is_a.$version EXP+HTP.P.is_a.$version EXP+HTP.C.is_a.$version
+	EXP+HTP.${version}.gz \
+	EXP+HTP.F.is_a.$version EXP+HTP.P.is_a.$version EXP+HTP.C.is_a.$version
     rm  EXP+HTP.${version}.gz
 done
 
-$bindir/make_target.py \
-    EXP.F.is_a.$old EXP.P.is_a.$old EXP.C.is_a.$old \
-    EXP.F.is_a.$new EXP.P.is_a.$new EXP.C.is_a.$new \
-    EXP_NK.F EXP_NK.P EXP_NK.C \
-    EXP_LK.F EXP_LK.P EXP_LK.C \
-    EXP_PK.F EXP_PK.P EXP_PK.C
+for evidence in `echo EXP HTP EXP+HTP`;do
+    $bindir/make_target.py \
+	${evidence}.F.is_a.$old ${evidence}.P.is_a.$old ${evidence}.C.is_a.$old \
+	${evidence}.F.is_a.$new ${evidence}.P.is_a.$new ${evidence}.C.is_a.$new \
+	${evidence}_NK.F        ${evidence}_NK.P        ${evidence}_NK.C \
+	${evidence}_LK.F        ${evidence}_LK.P        ${evidence}_LK.C \
+	${evidence}_PK.F        ${evidence}_PK.P        ${evidence}_PK.C
+    cat ${evidence}_NK.F ${evidence}_LK.F ${evidence}_PK.F > ${evidence}.F
+    cat ${evidence}_NK.P ${evidence}_LK.P ${evidence}_PK.P > ${evidence}.P
+    cat ${evidence}_NK.C ${evidence}_LK.C ${evidence}_PK.C > ${evidence}.C
+    rm ${evidence}.*.is_a* ${evidence}_*K.*
+done
 
-cat EXP_NK.F EXP_LK.F EXP_PK.F > EXP.F
-cat EXP_NK.P EXP_LK.P EXP_PK.P > EXP.P
-cat EXP_NK.C EXP_LK.C EXP_PK.C > EXP.C
-
-$bindir/make_target.py \
-    HTP.F.is_a.$old HTP.P.is_a.$old HTP.C.is_a.$old \
-    HTP.F.is_a.$new HTP.P.is_a.$new HTP.C.is_a.$new \
-    HTP_NK.F HTP_NK.P HTP_NK.C \
-    HTP_LK.F HTP_LK.P HTP_LK.C \
-    HTP_PK.F HTP_PK.P HTP_PK.C
-
-cat HTP_NK.F HTP_LK.F HTP_PK.F > HTP.F
-cat HTP_NK.P HTP_LK.P HTP_PK.P > HTP.P
-cat HTP_NK.C HTP_LK.C HTP_PK.C > HTP.C
-
-$bindir/make_target.py \
-    EXP+HTP.F.is_a.$old EXP+HTP.P.is_a.$old EXP+HTP.C.is_a.$old \
-    EXP+HTP.F.is_a.$new EXP+HTP.P.is_a.$new EXP+HTP.C.is_a.$new \
-    EXP+HTP_NK.F EXP+HTP_NK.P EXP+HTP_NK.C \
-    EXP+HTP_LK.F EXP+HTP_LK.P EXP+HTP_LK.C \
-    EXP+HTP_PK.F EXP+HTP_PK.P EXP+HTP_PK.C
-
-cat EXP+HTP_NK.F EXP+HTP_LK.F EXP+HTP_PK.F > EXP+HTP.F
-cat EXP+HTP_NK.P EXP+HTP_LK.P EXP+HTP_PK.P > EXP+HTP.P
-cat EXP+HTP_NK.C EXP+HTP_LK.C EXP+HTP_PK.C > EXP+HTP.C
-
-rm *EXP.*.is_a* *EXP_*K.*
-rm *HTP.*.is_a* *HTP_*K.*
+echo "| evidence | MF targets | BP targets | CC targets |" >  readme.md
+echo "| :--:     | :--:       | :--:       | :--:       |" >> readme.md
+for evidence in `echo EXP HTP EXP+HTP`;do
+    echo "| $evidence    |" `cat $evidence.F|wc -l` "      |" `cat $evidence.P|wc -l` "      |" `cat $evidence.C|wc -l` "      |" >> readme.md
+done
