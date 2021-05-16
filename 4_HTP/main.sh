e9e99855a6837030d8d23c6561c0823ce45d2a63
zcx <zcx@xenial>
2021-05-15 20:25:12 -0400

    make_target.py for EXP

--- 4_HTP/main.sh
+++ 4_HTP/main.sh
@@ -12,6 +12,9 @@ old=198
 new=203
 for version in `echo $old $new`;do
     $bindir/propagate_gaf_terms.py $datadir/go-basic.obo \
+            $datadir/goa_uniprot_all.EXP.${version}.gz   \
+	    EXP.F.is_a.$version EXP.P.is_a.$version EXP.C.is_a.$version
+    $bindir/propagate_gaf_terms.py $datadir/go-basic.obo \
         $datadir/goa_uniprot_all.HTP.${version}.gz       \
         HTP.F.is_a.$version HTP.P.is_a.$version HTP.C.is_a.$version
     zcat $datadir/goa_uniprot_all.EXP.${version}.gz      \
@@ -22,9 +25,16 @@ for version in `echo $old $new`;do
     rm  EXP+HTP.${version}.gz
 done
 
-cat $datadir/NK.F $datadir/LK.F $datadir/PK.F > EXP.F
-cat $datadir/NK.P $datadir/LK.P $datadir/PK.P > EXP.P
-cat $datadir/NK.C $datadir/LK.C $datadir/PK.C > EXP.C
+$bindir/make_target.py \
+    EXP.F.is_a.$old EXP.P.is_a.$old EXP.C.is_a.$old \
+    EXP.F.is_a.$new EXP.P.is_a.$new EXP.C.is_a.$new \
+    EXP_NK.F EXP_NK.P EXP_NK.C \
+    EXP_LK.F EXP_LK.P EXP_LK.C \
+    EXP_PK.F EXP_PK.P EXP_PK.C
+
+cat EXP_NK.F EXP_LK.F EXP_PK.F > EXP.F
+cat EXP_NK.P EXP_LK.P EXP_PK.P > EXP.P
+cat EXP_NK.C EXP_LK.C EXP_PK.C > EXP.C
 
 $bindir/make_target.py \
     HTP.F.is_a.$old HTP.P.is_a.$old HTP.C.is_a.$old \
@@ -48,4 +58,5 @@ cat EXP+HTP_NK.F EXP+HTP_LK.F EXP+HTP_PK.F > EXP+HTP.F
 cat EXP+HTP_NK.P EXP+HTP_LK.P EXP+HTP_PK.P > EXP+HTP.P
 cat EXP+HTP_NK.C EXP+HTP_LK.C EXP+HTP_PK.C > EXP+HTP.C
 
+rm *EXP.*.is_a* *EXP_*K.*
 rm *HTP.*.is_a* *HTP_*K.*
