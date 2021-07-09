f7c0729c720a8129d7076411c81f35ee63308957
chengxin zhang <zcx@login.expanse.sdsc.edu>
2021-07-08 18:53:52 -0700

    species specific naive

--- bin/predict_naive.py
+++ bin/predict_naive.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-docstring='''predict_naive.py target.list naive_1_all.txt 
+docstring='''predict_naive.py target.species _all.txt 
     use naive baseline to predict GO terms for input file target.fasta,
 '''
 import sys
@@ -21,13 +21,70 @@ def read_naive_prob():
         fp.close()
     return sorted(naive_GOterm_list,reverse=True)
 
-def read_target_list(infile):
-    fp=open(infile,'r')
-    target_list=fp.read().splitlines()
+def train_species_naive_prob(species):
+    species_naive_GOterm_list=[]
+    template_list=[]
+    fp=open(datdir+"/uniprot_sprot_exp.species",'r')
+    for line in fp.read().splitlines():
+        items=line.split()
+        if items[1]==species:
+            template_list.append(items[0])
     fp.close()
-    return target_list
+    if len(template_list)==0:
+        sys.stderr.write("0 template for species %s\n"%(species))
+        return []
+    sys.stderr.write("train for species %s on %d templates\n"%(
+        species,len(template_list)))
+    template_set=set(template_list)
 
-def write_output(target_list,naive_GOterm_list,outfile):
+    for Aspect in "FPC":
+        annotation_dict=dict()
+        GOterm_list=[]
+        fp=open(datdir+"/uniprot_sprot_exp."+Aspect,'r')
+        for line in fp.read().splitlines():
+            template,GOterms=line.split()
+            if template in template_set:
+                annotation_dict[template]=GOterms.split(',')
+                GOterm_list+=annotation_dict[template]
+                annotation_dict[template]=set(annotation_dict[template])
+        fp.close()
+
+        if len(annotation_dict)==0:
+            filename=datdir+"/naive."+Aspect
+            sys.stderr.write("0 template for %s aspect. fallback to %s\n"%(
+                Aspect,filename))
+            fp=open(filename,'r')
+            for line in fp.read().splitlines():
+                GOterm,cscore=line.split('\t')[:2]
+                if "%.2f"%float(cscore)=="0.00":
+                    break
+                species_naive_GOterm_list.append((float(cscore),GOterm))
+            fp.close()
+        else:
+            cscore_list=[]
+            for GOterm in list(set(GOterm_list)):
+                cscore=(1.+sum([GOterm in annotation_dict[template] for \
+                    template in annotation_dict]))/(1.+len(annotation_dict))
+                if "%.2f"%float(cscore)=="0.00":
+                    continue
+                cscore_list.append((cscore,GOterm))
+            cscore_list.sort(reverse=True)
+            species_naive_GOterm_list+=cscore_list
+    return sorted(species_naive_GOterm_list,reverse=True)
+
+def read_target_list(infile):
+    target_list=[]
+    target_species_dict=dict()
+    fp=open(infile,'r')
+    for line in fp.read().splitlines():
+        target,species=line.split()
+        target_list.append(target)
+        target_species_dict[target]=species
+    fp.close()
+    return target_list,target_species_dict
+
+def write_output(target_list,target_species_dict,naive_GOterm_list,
+        species_naive_GOterm_dict,suffix):
     txt ="AUTHOR naive\n"
     txt+="MODEL 1\n"
     txt+="KEYWORDS de novo prediction.\n"
@@ -35,7 +92,19 @@ def write_output(target_list,naive_GOterm_list,outfile):
         for cscore,GOterm in naive_GOterm_list:
             txt+="%s\t%s\t%.2f\n"%(target,GOterm,cscore)
     txt+="END\n"
-    fp=open(outfile,'w')
+    fp=open("naive_1"+suffix,'w')
+    fp.write(txt)
+    fp.close()
+    
+    txt ="AUTHOR naive\n"
+    txt+="MODEL 2\n"
+    txt+="KEYWORDS de novo prediction.\n"
+    for target in target_list:
+        species=target_species_dict[target]
+        for cscore,GOterm in species_naive_GOterm_dict[species]:
+            txt+="%s\t%s\t%.2f\n"%(target,GOterm,cscore)
+    txt+="END\n"
+    fp=open("naive_2"+suffix,'w')
     fp.write(txt)
     fp.close()
     return
@@ -45,8 +114,16 @@ if __name__=="__main__":
         sys.stderr.write(docstring)
         exit()
 
-    infile =sys.argv[1]
-    outfile=sys.argv[2]
+    infile=sys.argv[1]
+    suffix=sys.argv[2]
     naive_GOterm_list=read_naive_prob()
-    target_list=read_target_list(infile)
-    write_output(target_list,naive_GOterm_list,outfile)
+    target_list,target_species_dict=read_target_list(infile)
+    species_list=list(set(target_species_dict.values()))
+    print("%d species"%len(species_list))
+    species_naive_GOterm_dict=dict()
+    for species in species_list:
+        species_naive_GOterm_dict[species]=train_species_naive_prob(species)
+        if len(species_naive_GOterm_dict[species])==0:
+            species_naive_GOterm_dict[species]=naive_GOterm_list
+    write_output(target_list,target_species_dict,naive_GOterm_list,
+        species_naive_GOterm_dict,suffix)
